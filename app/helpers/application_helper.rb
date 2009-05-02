f97bf692d6f18b39dd8fc337e1a7cfd6f7035e25
Marko Anastasov <marko@renderedtext.com>
2009-05-03 01:06:59 +0200

    Add Sprawsm CSS framework, application layout

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,10 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+
+  def page_header(text)
+    content_for :page_header do
+      text
+    end
+  end
+
 end
