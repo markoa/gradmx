ccae4bc9862d559ebb54b9710f76638f3da8b023
Marko Anastasov <marko@renderedtext.com>
2009-05-31 19:51:45 +0200

    More unit testing for Presence and thus TokyoRecord

--- test/unit/presence_test.rb
+++ test/unit/presence_test.rb
@@ -14,12 +14,24 @@ class PresenceTest < ActiveSupport::TestCase
       assert_equal p.event_id, 1
       assert_equal Presence.last, p
       assert_equal Presence.find_by_key(p.key), p
+      assert Presence.all.include?(p)
 
       assert users(:quentin).presences
       assert users(:quentin).presences.include?(p)
 
+      assert_equal p.event, events(:one)
+      assert_equal p.user, users(:quentin)
+
       assert events(:one).presences
       assert events(:one).presences.include?(p)
+    end
+  end
+
+  test "should create presence with class method" do
+    assert_difference('Presence.count') do
+      p = Presence.create(:user_id => 1, :event_id => 1)
+      assert_equal p.user_id, 1
+      assert_equal p.event_id, 1
     end
   end
 
