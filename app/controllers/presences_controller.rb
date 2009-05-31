4a83e36804ee5cf35d32a888af527dfe3ed8ded9
Marko Anastasov <marko@renderedtext.com>
2009-05-31 20:03:12 +0200

    Decided that I won't allow XML access to Presences; functional test for create.

--- app/controllers/presences_controller.rb
+++ app/controllers/presences_controller.rb
@@ -3,7 +3,6 @@ class PresencesController < ApplicationController
   before_filter :login_required, :find_event
 
   # POST /events/1/presences
-  # TODO: POST /events/1/presences.xml
   def create
     existing = Presence.query do |q|
       q.add "user_id", :eq, @user.id.to_s
@@ -12,38 +11,21 @@ class PresencesController < ApplicationController
 
     if existing.empty?
       @presence = Presence.create(:event_id => @event.id, :user_id => @user.id)
-      respond_to do |format|
-        format.html do
-          flash[:notice] = "You have announced your presence"
-          redirect_to(@event)
-        end
-        # requires TokyoRecord#to_xml
-        #format.xml  { render :xml => @presence, :status => :created, :location => @event }
-      end
+      flash[:notice] = "You have announced your presence"
     else
-      respond_to do |format|
-        format.html do
-          flash[:notice] = "You have already announced your presence"
-          redirect_to(@event)
-        end
-        #format.xml  { render :xml => @presence, :status => :created, :location => @event }
-      end
+      flash[:notice] = "You have already announced your presence"
     end
+
+    redirect_to(@event)
   end
 
   # DELETE /events/1/presences/2
-  # DELETE /events/1/presences/2.xml
   def destroy
     @presence = Presence.find_by_key(params[:id])
     @presence.destroy if @presence
 
-    respond_to do |format|
-      format.html do
-        flash[:notice] = "You have cancelled your presence"
-        redirect_to(@event)
-      end
-      format.xml { head :ok }
-    end
+    flash[:notice] = "You have cancelled your presence"
+    redirect_to(@event)
   end
 
   protected
--- test/functional/presences_controller_test.rb
+++ test/functional/presences_controller_test.rb
@@ -1,8 +1,20 @@
 require 'test_helper'
 
 class PresencesControllerTest < ActionController::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
+
+  test "should create presence" do
+    login_as :quentin
+    assert_difference('Presence.count') do
+      e = events(:one)
+      post :create, { :event_id => e.id.to_s }
+      assert_equal assigns(:presence).event, e
+      assert_equal assigns(:presence).user, users(:quentin)
+      assert_redirected_to e
+    end
+  end
+
+  test "should delete presence" do
+    #TODO: figure out how to have seed data
     assert true
   end
 end
