a6bf3853e746491ba728842ceddbf5845dd99014
Marko Anastasov <marko@renderedtext.com>
2009-10-10 20:06:29 +0200

    Assert highlights list

--- test/functional/welcome_controller_test.rb
+++ test/functional/welcome_controller_test.rb
@@ -4,5 +4,6 @@ class WelcomeControllerTest < ActionController::TestCase
   test "should render home page" do
     get :index
     assert_response :success
+    assert_select '#highlights', true
   end
 end
