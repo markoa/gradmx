626b519ce81eea3929c769cc1c3afb0983779be5
Marko Anastasov <marko@renderedtext.com>
2010-03-10 12:30:28 +0100

    More notes, add faker as required, fix a test error.

--- config/environment.rb
+++ config/environment.rb
@@ -26,6 +26,7 @@ Rails::Initializer.run do |config|
   config.gem "will_paginate"
   config.gem "rufus-tokyo"
   config.gem "tyrantmanager"
+  config.gem "faker"
 
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
